b3530ff128ec3715c9391f05f83c2728a91f3836
Explore Student <explore-student@ip-172-31-39-108.eu-west-1.compute.internal>
2020-06-24 18:25:02 +0000

    update to markdown

---
+++ resources/streamlit_markdown/tweet_classifier_info.md
@@ -0,0 +1,1 @@
+This is markdown for the tweet classifier, not sure if html format though
--- stream_tut.py
+++ stream_tut.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import spacy
+import nltk
 
 #'''Will need to do POS tagging to filter tweets by organizations '''
 train_df = pd.read_csv('resources/train.csv')
 
-# models = ['model_1','model_2','model_3']
-# mod_select = st.selectbox('choose_option',models)
+
+models = {'LogisticRegressor':'Model_1','SupportVector':'model_2','RandomForrest':'Model_3'}
 
 def main():
     '''main function'''
@@ -21,18 +21,26 @@ def main():
     if selection == 'Information':
         st.sidebar.selectbox('type of info wanted',['models','presentation'])
         st.info('General Information')
-        st.markdown('read markdown file here')
+        app_info =open('resources/streamlit_markdown/tweet_classifier_info.md')
+        st.write(app_info.read())
         
         st.subheader('Raw Twitter data and label')
         if st.checkbox('Show raw data'):
             st.write(train_df[['sentiment','message']])
+            st.checkbox('show lemmatized data')
     if selection == 'Prediction':
         st.info('Prediction with ML models')
         tweet_text = st.text_area('Enter text','Type here')
         
         if st.button('Classify'):
-            models = ['model_1','model_2','model_3']
-            st.selectbox('select options',models)
+            model_select = st.selectbox('select models',list(models.keys()))
+    
+            if model_select == 'LogisticRegressor':
+                st.write(models[model_select]+str(3))
+            if model_select == 'SupportVector':
+                st.write(models[model_select]+str(2))
+            if model_select == 'RandomForrest':
+                st.write(models[model_select] + str(1))
 
 if __name__ == '__main__':
     main()
